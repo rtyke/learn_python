a9ee7ff4c4ab0f72fa698147fc27b8d2441b1336
rtyke <olga.goi@gmail.com>
2018-11-30 01:13:43 +0300

    add scores assignment

--- lesson02/scores.py
+++ lesson02/scores.py
@@ -1,14 +1,29 @@
 # https://learn.python.ru/lessons/13_for.html?full#16
 
-scores = [
+
+PERFOMANCE = [
     {'school_class': '4a', 'scores': [3, 4, 4, 5, 2]},
     {'school_class': '2b', 'scores': [5, 4, 4, 5, 5]},
     {'school_class': '9b', 'scores': [3, 3, 4, 4, 5]},
 ]
 
+
 def average_school_score():
-    pass
+    total_scores, scores_number = 0, 0
+    for school_class in PERFOMANCE:
+        total_scores += sum(school_class['scores'])
+        scores_number += len(school_class['scores'])
+    return total_scores/scores_number
 
 
 def average_class_score(class_name):
-    pass
+    for school_class in PERFOMANCE:
+        if school_class['school_class'] == class_name:
+            scores = school_class['scores']
+            return sum(scores)/len(scores)
+
+
+print(average_school_score())
+print(average_class_score('4a'))
+print(average_class_score('2b'))
+print(average_class_score('9b'))
